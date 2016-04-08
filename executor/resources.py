552bfb0fdcba5b6f240e96c3c44c969482311b94
Rostyslav <rostawesomegd@gmail.com>
2016-04-09 02:09:16 +0300

    API according to docs

--- executor/resources.py
+++ executor/resources.py
@@ -1,9 +1,30 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 class ProjectList(Resource):
     def get(self):
-        return {'list of':'resources', 'in format':'TDB'}
+        return {'list of':'projects', 'in format':'TDB'}
 
 class Project(Resource):
     def get(self, projectId):
         return {'project':projectId}
+
+class ProjectBuilds(Resource):
+    def get(self, projectId):
+        return {'List of builds for':projectId}
+
+class JobList(Resource):
+    def get(self):
+        return {'list of': 'jobs'}
+
+class Job(Resource):
+    def get(self, jobId):
+        return { 'job':jobId}
+
+class Build(Resource):
+    def post(self, action):
+        argParser = reqparse.RequestParser()
+        argParser.add_argument('projectId',
+                          dest='projectId',
+                          required=True)
+        args = argParser.parse_args()
+        return {'projectId':args.projectId}
--- executor/service.py
+++ executor/service.py
@@ -9,11 +9,17 @@ executorService = Flask("Executor")
 executorApi = Api(executorService)
 
 executorApi.add_resource(resources.ProjectList, '/projects')
-execturorApi.add_resource(resources.Project, '/projects/<int:projectId>')
+executorApi.add_resource(resources.Project, '/projects/<int:projectId>')
+executorApi.add_resource(resources.ProjectBuilds, '/projects/<int:projectId>/builds')
+
+executorApi.add_resource(resources.JobList, '/jobs')
+executorApi.add_resource(resources.Job, '/jobs/<int:jobId>')
+
+executorApi.add_resource(resources.Build, '/build/<string:action>')
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage: %s port" % sys.argv[0]
+    if len(sys.argv) != 3:
+        print "Usage: %s host port" % sys.argv[0]
         sys.exit(0)
-    executorApp.run(host='0.0.0.0', port=sys.argv[1])
+    executorService.run(host=sys.argv[1], port=sys.argv[2])
 
