a9fa18686aac2694ba1267eb42e30b598ef13d42
Rostyslav <rostawesomegd@gmail.com>
2016-04-28 23:14:30 +0300

    Refactor. Add ExecutorApp class

--- executor/__main__.py
+++ executor/__main__.py
@@ -1,6 +1,7 @@
 import sys
 from flask import Flask, got_request_exception
 from flask_restful import Api
+from pymongo import MongoClient
 
 import executor.Leaves as Leaves
 import executor.Projects as Projects
@@ -10,44 +11,69 @@ import executor.Config as Config
 from common.Project import ProjectConfig
 from common.Leaf import LeafConfig
 
+
 # TODO(yourash): implement colored mitli-level logging
 def logException(sender, exception, **extra):
     print('Exception occured: %s' % exception)
 
-if len(sys.argv) < 3:
-    print("Usage: %s host port" % sys.argv[0])
-    sys.exit(0)
+class ExecutorApp:
+    def __init__(self, host, port):
+        self.loadConfigs()
+        self.host = host
+        self.port = port
 
-if len(sys.argv) == 4:
-    Config.__configBasePath = sys.argv[3]
+        self.service = Flask('Executor')
+        self.service.config['BUNDLE_ERRORS'] = True
+        got_request_exception.connect(logException, self.service)
+        self.api = Api(self.service)
 
-if not Config.checkDirs():
-    sys.exit(1)
+        self.api.add_resource(Leaves.LeafIndex, '/leaves/')
+        self.api.add_resource(Leaves.LeafResource, '/leaves/<int:leafId>')
 
-Builds.storage = Builds.BuildDAO(None)
+        self.api.add_resource(Projects.ProjectIndex, '/projects/')
+        self.api.add_resource(Projects.ProjectResource, '/projects/<int:projectId>')
 
-for project in Config.getProjects():
-    Projects.storage.addProject(ProjectConfig(project))
+        self.api.add_resource(Builds.BuildIndex, '/builds/')
+        self.api.add_resource(Builds.BuildRecentIndex, '/builds/recent/')
+        self.api.add_resource(Builds.BuildResource, '/builds/<int:buildId>')
 
-for leaf in Config.getLeaves():
-    Leaves.storage.addLeaf(LeafConfig(leaf))
+    def run(self):
+        self.connectDb()
+        self.initStorages()
+        self.service.run(host=self.host, port=self.port)
 
 
-executorService = Flask("Executor")
-executorService.config['BUNDLE_ERRORS'] = True
-got_request_exception.connect(logException, executorService)
+    def loadConfigs(self):
+        Config.checkDirs()
+        # TODO: load Executor configuration from .palmate/executor.yaml
+        self.mongoHost='localhost'
+        self.mongoPort=27017
 
-executorApi = Api(executorService)
+    def initStorages(self):
+        for project in Config.getProjects():
+            Projects.storage.addProject(ProjectConfig(project))
 
-executorApi.add_resource(Leaves.LeafIndex, '/leaves/')
-executorApi.add_resource(Leaves.LeafResource, '/leaves/<int:leafId>')
+        for leaf in Config.getLeaves():
+            Leaves.storage.addLeaf(LeafConfig(leaf))
 
-executorApi.add_resource(Projects.ProjectIndex, '/projects/')
-executorApi.add_resource(Projects.ProjectResource, '/projects/<int:projectId>')
+        Builds.storage = Builds.BuildDAO(self.mongoClient['builds-db'])
 
-executorApi.add_resource(Builds.BuildIndex, '/builds/')
-executorApi.add_resource(Builds.BuildRecentIndex, '/builds/recent/')
-executorApi.add_resource(Builds.BuildResource, '/builds/<int:buildId>')
+    def connectDb(self):
+        # TODO(yourash): implement colored mitli-level logging
+        print('Connecting to mongodb at %s:%d' % (self.mongoHost, self.mongoPort))
+        self.mongoClient = MongoClient(self.mongoHost, self.mongoPort)
+        serverInfo = self.mongoClient.server_info()
+        print('Connected, server info:')
+        print(serverInfo)
 
-executorService.run(host=sys.argv[1], port=sys.argv[2])
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: %s host port" % sys.argv[0])
+        sys.exit(0)
+
+    if len(sys.argv) == 4:
+        Config.__configBasePath = sys.argv[3]
+
+    executor = ExecutorApp(sys.argv[1], sys.argv[2])
+    executor.run()
 
